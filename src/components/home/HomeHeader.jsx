1e691badc57a9a0c879d96f3eefc39f27aa7e455
EzequielRindello <109447017+EzequielRindello@users.noreply.github.com>
2025-07-07 09:51:07 -0300

    add custum h1

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -1,13 +1,21 @@
-const HomeHeader = ({ onCallToAction }) => (
-  <div className="col-12 text-center mb-4">
-    <h1 className="display-3">Welcome back!</h1>
-    <p className="fs-6">
-      System Statistics ·{" "}
-      <strong className="call-to-action-link" onClick={onCallToAction}>
-        View movies
-      </strong>
-    </p>
-  </div>
-);
+const HomeHeader = ({ onCallToAction }) => {
+  const name = JSON.parse(localStorage.getItem("user"))?.firstName;
+
+  return (
+    <div className="col-12 text-center mb-4">
+      {name ? (
+        <h1 className="display-4">Welcome back, {name}!</h1>
+      ) : (
+        <h1 className="display-4">Welcome to MOVIEapp!</h1>
+      )}
+      <p className="fs-6">
+        System Statistics ·{" "}
+        <strong className="call-to-action-link" onClick={onCallToAction}>
+          View movies
+        </strong>
+      </p>
+    </div>
+  );
+};
 
 export default HomeHeader;
