539acb699c5eb996376f90f169ef06b5e3991c98
EzequielRindello <109447017+EzequielRindello@users.noreply.github.com>
2025-07-10 12:06:57 -0300

    refactor FunctionCard to manage login state and conditionally render button

--- src/components/functions/FunctionCard.jsx
+++ src/components/functions/FunctionCard.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
-import {formatFunctionDateTime} from "../../services/formatters"
+import { formatFunctionDateTime } from "../../services/formatters";
 import FunctionModal from "../modals/FunctionModal";
 
 const FunctionCard = ({ func }) => {
   const [showModal, setShowModal] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(token ? true : false); 
+  }, []);
 
   const handleClick = () => {
     setShowModal(true);
@@ -23,11 +29,14 @@ const FunctionCard = ({ func }) => {
             {func.movie.directorName} - {formatFunctionDateTime(func)}
           </Card.Subtitle>
           <Card.Text>Price: ${func.price}</Card.Text>
-          <Button variant="danger" onClick={handleClick}>
-            Manage Function
-          </Button>
+          {isLoggedIn && (
+            <Button variant="danger" onClick={handleClick}>
+              Manage Function
+            </Button>
+          )}
         </Card.Body>
       </Card>
+
       <FunctionModal
         show={showModal}
         handleClose={handleClose}
